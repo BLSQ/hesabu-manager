9af71799830f047287462a6d143897a975130189
pjaspers <pjaspers@bluesquarehub.com>
2019-11-27 12:31:04 +0100

    SimulationHeader -> Simulation/Header
    noop. Ran prettier (with proptypes)

--- src/components/Simulation/Header.js
+++ src/components/Simulation/Header.js
@@ -2,10 +2,11 @@ import React, { Fragment } from "react";
 import humanize from "string-humanize";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles(theme => ({}));
 
-const SimulationHeader = function(props) {
+const Header = function(props) {
   const name = props.invoice.code;
   const classes = useStyles(props);
   return (
@@ -26,4 +27,12 @@ const SimulationHeader = function(props) {
   );
 };
 
-export default SimulationHeader;
+Header.propTypes = {
+  invoice: PropTypes.shape({
+    code: PropTypes.string,
+    period: PropTypes.string,
+    orgunit_ext_id: PropTypes.string,
+  }),
+};
+
+export default Header;
