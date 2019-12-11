be38dc00b2967ed4612865d191b92ab9614727cb
pjaspers <pjaspers@bluesquarehub.com>
2019-12-11 11:55:50 +0100

    Add multiple simulations to mock
    `/api/simulations/1` => Normal result
    `/api/simulations/2` => Normal result (but different period)
    `/api/simulations/3` => Error: Could not connect do DHIS2

---
+++ mock-server/README.md
@@ -0,0 +1,4 @@
+
+`/api/simulations/1` => Normal result
+`/api/simulations/2` => Normal result (but different period)
+`/api/simulations/3` => Error: Could not connect do DHIS2
--- mock-server/app.rb
+++ mock-server/app.rb
@@ -9,12 +9,21 @@ get '/' do
   'Hello world!'
 end
 
-get '/api/simulations/:identifier' do
+get '/api/simulations/:identifier' do |identifier|
+  puts identifier
   content_type :json
-  File.read('data/simulation-result.json')
+  if path = Dir.glob("data/*#{identifier}.json").first
+    File.read(path)
+  else
+    File.read('data/simulation-result-1.json')
+  end
 end
 
-get '/s3/result' do
+get '/s3/results/:identifier.json' do |identifier|
   content_type :json
-  File.read('data/s3-result.json')
+  if path = Dir.glob("data/s3/*#{identifier}.json").first
+    File.read(path)
+  else
+    File.read('data/s3/success-1.json')
+  end
 end
