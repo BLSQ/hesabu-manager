d517c8723b83d3c9064160155dcd088d59f31379
Piet Jaspers <pjaspers@bluesquarehub.com>
2019-12-12 09:59:52 +0100

    feat: Add `simulations` endpoint and direct link for simulations (#24)
    * Add `simulations` endpoint and direct link for simulations
    
    * Move to /api/simulation with parameters
    
    Less confusing to read.

--- mock-server/app.rb
+++ mock-server/app.rb
@@ -1,6 +1,22 @@
 # myapp.rb
 require 'sinatra'
 
+def simulation_id_based_on_org_unit_and_period(org_unit, period)
+  all = JSON.parse(File.read("data/simulations.json"))
+  response = all["data"].detect do |h|
+    puts h
+    puts h["attributes"]["orgUnit"]
+    puts h["attributes"]["dhis2Period"]
+    h["attributes"]["orgUnit"] == org_unit &&
+      h["attributes"]["dhis2Period"] == period
+  end
+  if response
+    response["id"]
+  else
+    nil
+  end
+end
+
 before do
   response.headers['Access-Control-Allow-Origin'] = '*'
 end
@@ -9,9 +25,29 @@ get '/' do
   'Hello world!'
 end
 
-get '/api/simulations/:identifier' do |identifier|
-  puts identifier
+get '/api/simulations' do
   content_type :json
+  File.read("data/simulations.json")
+end
+
+# Filter to a specific simulation
+#
+# Needs ?orgUnit=bla&periods=2019Q1
+get '/api/simulation' do
+  org_unit = params[:orgUnit] || ""
+  period = params[:periods].split(",").first
+  identifier = simulation_id_based_on_org_unit_and_period(org_unit, period)
+
+  if identifier && path = Dir.glob("data/*#{identifier}.json").first
+    File.read(path)
+  else
+    not_found
+  end
+end
+
+get %r{/api/simulations/([0-9]+)} do |identifier|
+  content_type :json
+  periods = params[:periods]
   if path = Dir.glob("data/*#{identifier}.json").first
     File.read(path)
   else
