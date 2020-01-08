1e7e191a1b595fa313b0ad2c9945395a5c4586e5
Boris Rorsvort <boris@rorsvort.com>
2020-01-08 14:03:45 +0100

    fix: Fix the responsive drawer toggle

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,12 +1,15 @@
-import { SET_SELECTED_CELL } from "../constants/types";
+import { SET_SELECTED_CELL, TOGGLE_DRAWER } from "../constants/types";
 
 function ui(
   state = {
+    drawerOpen: false,
     selectedCell: undefined,
   },
   action,
 ) {
   switch (action.type) {
+    case TOGGLE_DRAWER:
+      return { ...state, drawerOpen: !state.drawerOpen };
     case SET_SELECTED_CELL:
       return { ...state, selectedCell: action.cell };
     default:
