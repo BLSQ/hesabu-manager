fda92c36a486565a70b2e1a3878d497273f30874
Boris Rorsvort <boris@rorsvort.com>
2020-01-15 12:56:40 +0100

    first draft for data grid

--- src/containers/SetCurrentLevelContainer/index.js
+++ src/containers/SetCurrentLevelContainer/index.js
@@ -1,20 +1,89 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import ReactDataSheet from "react-datasheet";
+import PropTypes, { number } from "prop-types";
 import { APPBAR_WITH_TABS_HEIGHT } from "../../constants/ui";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: APPBAR_WITH_TABS_HEIGHT,
+    marginTop: APPBAR_WITH_TABS_HEIGHT - theme.spacing(2),
+    marginLeft: theme.spacing(-2),
     padding: theme.spacing(2),
   },
 }));
 
+function fakeColumGenerator(number, readOnly = false) {
+  const items = [];
+  for (let index = 0; index < number; index++) {
+    items.push({
+      value: "",
+      readOnly,
+    });
+  }
+  return items;
+}
+
+function fakeRowGenerator(number, fakeColumn) {
+  const items = [];
+  for (let index = 0; index < number; index++) {
+    items.push(fakeColumn);
+  }
+  return items;
+}
+
 const SetCurrentLevelContainer = props => {
   const classes = useStyles(props);
+  const { topics, inputs } = props.set;
+  const safeTopics = topics || [];
+  const safeInputs = inputs || [];
+  const defaultGrid = [
+    [
+      { value: "", disableEvents: true, readOnly: true },
+      ...safeInputs.map(input => ({
+        value: input.name,
+        disableEvents: true,
+        readOnly: true,
+      })),
+      ...fakeColumGenerator(10, true),
+    ],
+  ];
+  const grid = [
+    ...defaultGrid,
+    ...safeTopics.map(topic => [
+      {
+        value: topic.name,
+        readOnly: true,
+        disableEvents: true,
+        noWrap: true,
+      },
+      ...safeInputs.map(() => ({
+        value: "",
+      })),
+      ...fakeColumGenerator(10),
+    ]),
+    ...fakeRowGenerator(
+      50,
+      fakeColumGenerator(safeInputs.length + 11, false),
+      true,
+    ),
+  ];
 
-  return <div className={classes.root}>CurrentLevelContainer</div>;
+  if (grid.length) {
+    return (
+      <div className={classes.root}>
+        <ReactDataSheet
+          data={grid}
+          valueRenderer={cell => cell.value}
+          attributesRenderer={cell => ({ "data-nowrap": cell.noWrap || {} })}
+        />
+      </div>
+    );
+  }
+  return null;
 };
 
-SetCurrentLevelContainer.propTypes = {};
+SetCurrentLevelContainer.propTypes = {
+  set: PropTypes.object,
+};
 
 export default SetCurrentLevelContainer;
