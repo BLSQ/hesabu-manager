1576146ea72720ec7f5f0a65caeb223902c841ed
Boris Rorsvort <boris@rorsvort.com>
2020-01-24 12:17:40 +0100

    wip input mapping display

--- src/containers/SetCurrentLevelContainer/index.js
+++ src/containers/SetCurrentLevelContainer/index.js
@@ -3,11 +3,24 @@ import { makeStyles } from "@material-ui/styles";
 import ReactDataSheet from "react-datasheet";
 import PropTypes from "prop-types";
 import { APPBAR_WITH_TABS_HEIGHT } from "../../constants/ui";
+import { getIn } from "formik";
 import SectionLoading from "../../components/Shared/SectionLoading";
 import {
   fakeColumGenerator,
   fakeRowGenerator,
 } from "../../utils/dataGridUtils";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  DialogContentText,
+  Chip,
+} from "@material-ui/core";
+import CloudDownload from "@material-ui/icons/CloudDownload";
+import CheckCircle from "@material-ui/icons/CheckCircle";
+import Transition from "../../components/Shared/Transition";
 
 const useStyles = makeStyles(theme => ({
   root: props => ({
@@ -18,11 +31,45 @@ const useStyles = makeStyles(theme => ({
   }),
 }));
 
+const DialogEditor = props => {
+  console.log("rpops", props);
+
+  return (
+    <Dialog open={true} TransitionComponent={Transition}>
+      <DialogTitle id="form-dialog-title">
+        {props.cell.inputMapping.name}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <Chip label={props.cell.inputMapping.kind} />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, quia
+            natus nesciunt necessitatibus exercitationem inventore autem
+            consectetur modi voluptas blanditiis nam incidunt esse debitis
+            veniam similique repellat eveniet architecto ullam?
+          </p>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary">Cancel</Button>
+        <Button color="primary">Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 const SetCurrentLevelContainer = props => {
   const classes = useStyles(props);
   const { topics, inputs } = props.set;
   const safeTopics = topics || [];
   const safeInputs = inputs || [];
+
+  const findInputMapping = (input, inputMappings) => {
+    return inputMappings.find(
+      inputMapping => inputMapping.input.id === input.id,
+    );
+  };
+
   const defaultGrid = [
     [
       { value: "", disableEvents: true, readOnly: true },
@@ -34,6 +81,7 @@ const SetCurrentLevelContainer = props => {
       ...fakeColumGenerator(10, true),
     ],
   ];
+
   const grid = [
     ...defaultGrid,
     ...safeTopics.map(topic => [
@@ -43,9 +91,16 @@ const SetCurrentLevelContainer = props => {
         disableEvents: true,
         noWrap: true,
       },
-      ...safeInputs.map(() => ({
-        value: "",
-      })),
+      ...safeInputs.map(input => {
+        const inputMapping = findInputMapping(input, topic.inputMappings);
+
+        return {
+          value: getIn(inputMapping, "name"),
+          inputMapping,
+          valueViewer: inputMapping ? CheckCircle : CloudDownload,
+          dataEditor: DialogEditor,
+        };
+      }),
       ...fakeColumGenerator(10),
     ]),
     ...fakeRowGenerator(
