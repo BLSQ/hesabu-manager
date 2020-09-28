d1dd0f991af3c9189f7139e408e9b196e8b5a534
Stéphan Mestach <smestach@bluesquare.org>
2020-09-28 17:19:56 +0200

    Display data element and category option combo or indicator names

--- src/components/Simulation/ExpandableCellContent/ViewOnDhis2Btn/index.js
+++ src/components/Simulation/ExpandableCellContent/ViewOnDhis2Btn/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Button, makeStyles } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { Dhis2Icon } from "@blsq/manager-ui";
 
 import camelCase from "lodash/camelCase";
+
+import Api from "../../../../lib/Api";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,38 +21,78 @@ const ViewOnDhis2Btn = props => {
   const { cell } = props;
   const classes = useStyles(props);
   const { t } = useTranslation();
+  const [identifiableObject, setIdentifiableObject] = useState(undefined);
 
   const isInput = !!cell.state;
   const isOutput = !!cell.dhis2_data_element;
-  let href;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const api = await Api.instance();
+      try {
+        setIdentifiableObject(undefined);
+        let dhis2Object = undefined;
+        if (isInput) {
+          const ids = cell.state.ext_id.replace("inlined-", "").split(".");
+          const dataElementId = ids[0]; // data element or indicator id
+          const cocId = ids[1]; // optional combo
+          dhis2Object = await api.get(`identifiableObjects/${dataElementId}`);
+          if (cocId) {
+            dhis2Object.categoryOptionCombo = await api.get(
+              `categoryOptionCombos/${cocId}`,
+              { fields: "id,name" },
+            );
+          }
+        }
+
+        if (isOutput) {
+          dhis2Object = await api.get(
+            `identifiableObjects/${cell.dhis2_data_element}`,
+          );
+        }
+        let dhis2UiUrl = undefined;
+
+        if (dhis2Object && dhis2Object.href.includes("/dataElements/")) {
+          dhis2UiUrl = `/dhis-web-maintenance/index.html#/edit/dataElementSection/dataElement/${dhis2Object.id}`;
+        }
+
+        if (dhis2Object && dhis2Object.href.includes("/indicators/")) {
+          dhis2UiUrl = `/dhis-web-maintenance/index.html#/edit/indicatorSection/indicator/${dhis2Object.id}`;
+        }
+
+        if (dhis2UiUrl) {
+          dhis2Object.dhis2UiUrl = api.baseUrl + "/.." + dhis2UiUrl;
+        }
+
+        setIdentifiableObject(dhis2Object);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [cell]);
 
   if (!isInput && !isOutput) {
     return null;
   }
 
-  if (isInput) {
-    href = `${
-      process.env.REACT_APP_DHIS2_URL
-    }/dhis-web-maintenance/index.html#/edit/${camelCase(
-      cell.state.kind,
-    )}Section/${camelCase(cell.state.kind)}/${cell.state.ext_id}`;
-  }
-
-  if (isOutput) {
-    href = `${process.env.REACT_APP_DHIS2_URL}/dhis-web-maintenance/index.html#/edit/dataElementSection/dataElement/${cell.dhis2_data_element}`;
-  }
-
   return (
-    <Button
-      tag="a"
-      href={href}
-      color="primary"
-      target="_blank"
-      className={classes.root}
-    >
-      <Dhis2Icon className={classes.icon} />
-      {t("buttons.seeOnDhis2")}
-    </Button>
+    <>
+      <Button
+        tag="a"
+        href={identifiableObject && identifiableObject.dhis2UiUrl}
+        color="primary"
+        target="_blank"
+        className={classes.root}
+      >
+        <Dhis2Icon className={classes.icon} />
+        {identifiableObject && identifiableObject.name}
+        {identifiableObject &&
+          identifiableObject.categoryOptionCombo &&
+          " : " + identifiableObject.categoryOptionCombo.name}
+      </Button>
+    </>
   );
 };
 
