3bff5c71925895bbe810d46f83f2532764c6554b
Stéphan Mestach <smestach@bluesquare.org>
2020-10-02 08:44:02 +0200

    Extract data load from useEffect

--- src/containers/Dhis2DataElementsProvider.js
+++ src/containers/Dhis2DataElementsProvider.js
@@ -9,6 +9,29 @@ import keyBy from "lodash/keyBy";
   here to populate the store with data elements and indicator cache for faster display within the children
   see ../lib/dhis2lookups{*} for pratical usage
 */
+
+const loadData = async () => {
+  const api = await Api.instance();
+  const deResp = await api.get("dataElements", {
+    fields: "id,name,valueType,aggregationType,dimensionItemType",
+    filter: "domainType:eq:AGGREGATE",
+    paging: false,
+  });
+  const indResp = await api.get("indicators", {
+    fields: "id,name,numerator,dimensionItemType",
+    paging: false,
+  });
+  const cocResp = await api.get("categoryOptionCombos", {
+    fields: "id,name,dimensionItemType",
+    paging: false,
+  });
+  return {
+    dataElements: keyBy(deResp.dataElements, "id"),
+    indicators: keyBy(indResp.indicators, "id"),
+    categoryOptionCombos: keyBy(cocResp.categoryOptionCombos, "id"),
+  };
+};
+
 const Dhis2DataElementsProvider = props => {
   const dispatch = useDispatch();
   const store = useStore();
@@ -16,27 +39,8 @@ const Dhis2DataElementsProvider = props => {
     const fetchDataElements = async () => {
       if (store.getState().dhis2.status === null) {
         dispatch(dhis2RequestDe());
-        const api = await Api.instance();
-        const deResp = await api.get("dataElements", {
-          fields: "id,name,valueType,aggregationType,dimensionItemType",
-          filter: "domainType:eq:AGGREGATE",
-          paging: false,
-        });
-        const indResp = await api.get("indicators", {
-          fields: "id,name,numerator,dimensionItemType",
-          paging: false,
-        });
-        const cocResp = await api.get("categoryOptionCombos", {
-          fields: "id,name,dimensionItemType",
-          paging: false,
-        });
-        dispatch(
-          dhis2ReceiveDe({
-            dataElements: keyBy(deResp.dataElements, "id"),
-            indicators: keyBy(indResp.indicators, "id"),
-            categoryOptionCombos: keyBy(cocResp.categoryOptionCombos, "id"),
-          }),
-        );
+
+        dispatch(dhis2ReceiveDe(await loadData()));
       }
     };
     fetchDataElements();
