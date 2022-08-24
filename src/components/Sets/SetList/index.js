15d77650a7e9440bc1f5f0ee863cc890e365c634
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-08-24 12:11:17 +0200

    adds fab for navigation to new set

--- src/components/Sets/SetList/index.js
+++ src/components/Sets/SetList/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
+import { makeStyles } from "@material-ui/core";
 import SetListItem from "../SetListItem";
 import EmptySection from "../../EmptySection";
+import ActionFab from "../../Shared/ActionFab";
+
+const useStyles = makeStyles(theme => ({
+  simulationBtn: {
+    right: theme.spacing(4) + 350,
+    transition: "all .1s 100ms ease-in-out",
+  },
+}));
 
 const SetList = props => {
   const { t } = useTranslation();
+  const classes = useStyles();
+
   if (!props.loading && props.noItems)
     return <EmptySection resourceName={t("resources.set")} />;
   if (!props.loading && !props.sets.length) return <p>No sets found</p>;
@@ -14,7 +25,12 @@ const SetList = props => {
       {props.sets.map((set, index) => (
         <SetListItem key={index} {...set} />
       ))}
-      <a href="./#/sets/new">new set</a>
+      <ActionFab
+        to={{ pathname: `${window.location.href.split("#")[1]}/new` }}
+        text="Set"
+        className={classes.simulationBtn}
+        extended
+      />
     </div>
   );
 };
