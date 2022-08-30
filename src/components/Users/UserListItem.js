dfdd54f442e8089c02d665551093ce0689c23250
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-08-30 13:36:30 +0200

    makes user list item prettier

--- src/components/Users/UserListItem.js
+++ src/components/Users/UserListItem.js
@@ -6,6 +6,7 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  Grid,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import EditIcon from "@material-ui/icons/Edit";
@@ -37,33 +38,52 @@ const UserListItem = ({ user }) => {
   return (
     <div className={classes.root}>
       <div className={classes.header}>
-        <Typography className={classes.sectionTitle}>{user.email}</Typography>
-        <div>
-          <IconButton
-            aria-label="edit user"
-            component="label"
-            onClick={openEditUser}
-          >
-            <EditIcon />
-          </IconButton>
-          <Dialog
-            open={editUser}
-            onClose={closeEditUser}
-            fullWidth
-            maxWidth="md"
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <DialogTitle>Edit user</DialogTitle>
-            <DialogContent style={{ height: "300px" }}>
-              <UserForm
-                user={user}
-                afterMutate={closeEditUser}
-                modeCreate={false}
-              />
-            </DialogContent>
-          </Dialog>
-        </div>
+        <Grid container justifyContent="space-between">
+          <Grid item>
+            <Typography className={classes.sectionTitle}>
+              {user.email}
+            </Typography>
+            <Grid container>
+              <Grid item>
+                {user.dhis2UserRef && (
+                  <>DHIS2 user reference: {user.dhis2UserRef}</>
+                )}
+
+                {!user.dhis2UserRef && (
+                  <>
+                    <b>DHIS2 reference not configured for this user</b>
+                  </>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <IconButton
+              aria-label="edit user"
+              component="label"
+              onClick={openEditUser}
+            >
+              <EditIcon fontSize="small" />
+            </IconButton>
+            <Dialog
+              open={editUser}
+              onClose={closeEditUser}
+              fullWidth
+              maxWidth="md"
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <DialogTitle>Edit user</DialogTitle>
+              <DialogContent style={{ height: "300px" }}>
+                <UserForm
+                  user={user}
+                  afterMutate={closeEditUser}
+                  modeCreate={false}
+                />
+              </DialogContent>
+            </Dialog>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
