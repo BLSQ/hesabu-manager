d90c0600db4b82865262409f723210c139f17497
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-08-30 13:07:02 +0200

    creates route for new user page, adds action fab

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 import UserListItem from "./UserListItem";
+import ActionFab from "../Shared/ActionFab";
+import { canEdit } from "../../actions/api";
+
+const useStyles = makeStyles(theme => ({
+  simulationBtn: {
+    right: theme.spacing(4) + 350,
+    transition: "all .1s 100ms ease-in-out",
+  },
+}));
 
 const UserList = ({ users }) => {
+  const classes = useStyles();
+  const userCanEdit = canEdit();
   return (
     <div>
       {users.map((user, index) => (
         <UserListItem key={index} user={user} />
       ))}
+      <ActionFab
+        disabled={!userCanEdit}
+        to={{ pathname: `${window.location.href.split("#")[1]}/new` }}
+        text="User"
+        className={classes.simulationBtn}
+        extended
+      />
     </div>
   );
 };
