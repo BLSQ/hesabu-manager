1d9d704e7f8222aa7e36f46f595a4b5ea1df1f3a
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-09-09 14:00:51 +0200

    loads compounds with react query

--- src/containers/CompoundsContainer/index.js
+++ src/containers/CompoundsContainer/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 import matchSorter from "match-sorter";
 import Compounds from "../../components/Compounds";
 import { deserialize } from "../../utils/jsonApiUtils";
@@ -8,30 +9,31 @@ const CompoundsContainer = () => {
   const [sideSheetOpen, setSideSheetOpen] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
   const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [compounds, setCompounds] = useState([]);
 
-  useEffect(() => {
-    setLoading(true);
-    externalApi()
-      .errorType("json")
-      .url(`/compounds`)
-      .get()
-      .json(response => {
-        setLoading(false);
-        deserialize(response).then(data => {
-          setCompounds(data);
-        });
-
+  const fetchCompoundsQuery = useQuery(
+    "fetchCompounds",
+    async () => {
+      let response = await externalApi()
+        .errorType("json")
+        .url(`/compounds`)
+        .get()
+        .json();
+      response = deserialize(response);
+      return response;
+    },
+    {
+      onSuccess: () => {
         setErrorMessage(null);
-      })
-      .catch(e => {
-        setErrorMessage(e.message);
-        setLoading(false);
-        setCompounds([]);
-      });
-  }, []);
+      },
+      onError: error => {
+        setErrorMessage(error.message);
+      },
+    },
+  );
+
+  const compounds = fetchCompoundsQuery?.data;
+  const loading = fetchCompoundsQuery?.isLoading;
 
   const handleToggleSideSheet = () => setSideSheetOpen(!sideSheetOpen);
   const handleToggleSearch = () => setSearchOpen(!searchOpen);
@@ -41,18 +43,22 @@ const CompoundsContainer = () => {
   });
 
   return (
-    <Compounds
-      filteredCompounds={filteredCompounds}
-      compounds={compounds}
-      loading={loading}
-      query={query}
-      setQuery={setQuery}
-      searchOpen={searchOpen}
-      errorMessage={errorMessage}
-      handleToggleSearch={handleToggleSearch}
-      sideSheetOpen={sideSheetOpen}
-      handleToggleSideSheet={handleToggleSideSheet}
-    />
+    <>
+      {compounds && (
+        <Compounds
+          filteredCompounds={filteredCompounds}
+          compounds={compounds}
+          loading={loading}
+          query={query}
+          setQuery={setQuery}
+          searchOpen={searchOpen}
+          errorMessage={errorMessage}
+          handleToggleSearch={handleToggleSearch}
+          sideSheetOpen={sideSheetOpen}
+          handleToggleSideSheet={handleToggleSideSheet}
+        />
+      )}
+    </>
   );
 };
 
