0a5125ddba818b8eae81cc69262017b3ccb9fbe3
Stéphan Mestach <smestach@bluesquare.org>
2022-10-11 08:45:13 +0200

    Just use function based component

--- src/components/Changes/SplitView.js
+++ src/components/Changes/SplitView.js
@@ -1,16 +1,12 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import ReactDiffViewer from "react-diff-viewer";
 
-class SplitView extends PureComponent {
-  render = () => {
-    return (
-      <ReactDiffViewer
-        oldValue={this.props.before.replace(/\r\n/g, "\n")}
-        newValue={this.props.after.replace(/\r\n/g, "\n")}
-        splitView={true}
-      />
-    );
-  };
-}
+const SplitView = props => (
+  <ReactDiffViewer
+    oldValue={props.before.replace(/\r\n/g, "\n")}
+    newValue={props.after.replace(/\r\n/g, "\n")}
+    splitView={true}
+  />
+);
 
 export default SplitView;
